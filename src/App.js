ae2b6f553e7742c5b63f1e7da8f8ca97fdbeee73
Lucas Pacheco <lucaspsilveira@live.com>
2019-04-13 19:40:58 -0300

    Adicionado componente para visualização do produto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import Toutes from './routes';
 
 import "./styles.css";
 
 import Header from './components/Header';
 import Main from './pages/main';
+import Routes from './routes';
 
 const App = () => {
   return (
     <div className="App">
         <Header />
-        <Main />
+        <Routes />
       </div>
   );
 };
