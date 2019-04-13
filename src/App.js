42fe3b45310043ee44c7f94c6b75bc262726838a
Lucas Pacheco <lucaspsilveira@live.com>
2019-04-13 17:16:57 -0300

    Listando produtos da API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
+import "./styles.css";
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+const App = () => {
+  return (
+    <div className="App">
+        <Header />
+        <Main />
       </div>
-    );
-  }
-}
+  );
+};
+
 
 export default App;
